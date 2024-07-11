6739bbdaa1de02f953b54607b822708d3f61cff8
Arteiimis <lionelym0987@gmail.com>
2024-07-12 00:04:48 +0800

    feat: Add SQLite connection configuration for demo backend
    This commit adds the necessary configuration for connecting to a SQLite database in the demo backend. The connection settings are specified in the `.vscode/settings.json` file.
    
    The `sqltools.connections` array now includes a new connection object with the following properties:
    - `previewLimit`: Specifies the maximum number of rows to display in the SQL query preview.
    - `driver`: Specifies the database driver to use, which is set to "SQLite".
    - `name`: Specifies the name of the connection, which is set to "democonn".
    - `database`: Specifies the path to the SQLite database file, which is set to "${workspaceFolder:admin_demo}/demo_backend/demo.db".
    
    This configuration enables the demo backend to establish a connection to the SQLite database and perform database operations.

--- demo_frontend/src/App.vue
+++ demo_frontend/src/App.vue
@@ -1,85 +1,158 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
+import type { Student, StudentCreate } from "./types";
+import { onMounted, ref } from "vue";
+import {
+  createStudent,
+  deleteStudent,
+  getStudent,
+  getStudentByName,
+  getStudents,
+  updateStudent,
+} from "./services/studentService";
+import { ElMessage } from "element-plus";
+
+const student = ref<Student | null>(null);
+const students = ref<Student[] | null>(null);
+const studentForm = ref<StudentCreate>({
+  student_id: 0,
+  name: "",
+  major: "",
+  status: "",
+});
+
+const stutas = [
+  { label: "在读", value: "在读" },
+  { label: "毕业", value: "毕业" },
+  { label: "休学", value: "休学" },
+];
+
+const majors = [
+  { label: "计算机", value: "计算机" },
+  { label: "数学", value: "数学" },
+  { label: "物理", value: "物理" },
+];
+
+onMounted(async () => {
+  students.value = await getStudents();
+});
+
+function genRandID() {
+  return Math.floor(Math.random() * 1000000);
+}
+
+async function handleSubmit() {
+  try {
+    if (studentForm.value.name === "") {
+      ElMessage.error("name is required");
+      return;
+    }
+    studentForm.value.student_id = genRandID();
+    await createStudent(studentForm.value);
+    studentForm.value = {
+      student_id: 0,
+      name: "",
+      major: "",
+      status: "",
+    };
+    students.value = await getStudents();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function handleDeleate(id: number) {
+  try {
+    await deleteStudent(id);
+    students.value = await getStudents();
+  } catch (error) {
+    console.error(error);
+  }
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class="margin_both">
+    <el-card class="margin_bottom">
+      <el-form :model="studentForm" label-width="auto">
+        <el-row justify="start" :gutter="10">
+          <el-col :span="7">
+            <el-form-item label="Name">
+              <el-input
+                v-model="studentForm.name"
+                placeholder="name"
+                clearable
+              />
+            </el-form-item>
+          </el-col>
+          <el-col :span="7">
+            <el-form-item label="Major">
+              <el-select
+                v-model="studentForm.major"
+                size="middle"
+                placeholder="major"
+                clearable
+              >
+                <el-option
+                  v-for="item in majors"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value"
+                ></el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="7">
+            <el-form-item label="Status">
+              <el-select
+                v-model="studentForm.status"
+                size="middle"
+                placeholder="status"
+                clearable
+              >
+                <el-option
+                  v-for="item in stutas"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value"
+                ></el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="3">
+            <el-button type="primary" @click="handleSubmit"> submit </el-button>
+          </el-col>
+        </el-row>
+      </el-form>
+    </el-card>
+    <el-card>
+      <el-table :data="students" style="width: 100%">
+        <el-table-column label="id" prop="student_id" />
+        <el-table-column label="name" prop="name" />
+        <el-table-column label="major" prop="major" />
+        <el-table-column label="status" prop="status" />
+        <el-table-column label="操作" width="200" align="center">
+          <template #default="{ row }">
+            <el-button type="primary" @click="handleUpdate(row.student_id)"
+              >修改</el-button
+            >
+            <el-button type="danger" @click="handleDeleate(row.student_id)"
+              >删除</el-button
+            >
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+.margin_both {
+  margin: auto 20px;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.margin_bottom {
+  margin-bottom: 10px;
 }
 </style>
